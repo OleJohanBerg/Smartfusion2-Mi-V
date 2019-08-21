e7a06f8f5312512fb5590ddc5e05b5e86ff2ddc5
OleJohanBerg <olejohanberg@gmail.com>
2019-08-21 14:19:41 +0200

    added firmware folder

---
+++ firmware/README.txt
@@ -0,0 +1,6 @@
+Export Firmware README
+
+Microsemi Corporation - Microsemi Libero Software Release v12.1 (Version 12.600.0.14)
+
+Date    :    Tue Aug  6 14:40:16 2019
+Project :    C:\RISC-V\mi-v
---
+++ firmware/top_hw_platform.h
@@ -0,0 +1,25 @@
+#ifndef top_HW_PLATFORM_H_
+#define top_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Tue Aug  6 14:40:16 2019
+*
+*Memory map specification for peripherals in top
+*/
+
+/*-----------------------------------------------------------------------------
+* MIV_RV32IMA_L1_AHB_C0_0 subsystem memory map
+* Master(s) for this subsystem: MIV_RV32IMA_L1_AHB_C0_0 
+*---------------------------------------------------------------------------*/
+#define COREAHBLSRAM_C0_0               0x80000000U
+
+
+/*-----------------------------------------------------------------------------
+* MIV_RV32IMA_L1_AHB_C0_0 subsystem memory map
+* Master(s) for this subsystem: MIV_RV32IMA_L1_AHB_C0_0 
+*---------------------------------------------------------------------------*/
+#define COREGPIO_IN                     0x70001000U
+#define COREGPIO_OUT                    0x70002000U
+
+
+#endif /* top_HW_PLATFORM_H_*/
